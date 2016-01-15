dbb208104acf8e883310b88c44edfa04bbe3672a
congdv <congdaovan94@gmail.com>
2016-01-15 22:31:21 +0700

    exersice day 2 part 3 cout word

---
+++ wordcount.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.7
+import sys
+def read_file(file_name):
+    word_count = {}
+    f = open(file_name,'r')
+    for line in f:
+        words = line.rstrip('\n').split(' ')
+        for word in words:
+            if word not in word_count.keys():
+                word_count[word] = 1
+            else:
+                word_count[word] = word_count[word] + 1
+    for word in word_count:
+        print word + ' %d'%word_count[word]
+def main():
+    if len(sys.argv) == 2:
+        read_file(sys.argv[1])
+    else:
+        read_file('blogger.txt')
+
+if __name__ == '__main__':
+    main()
