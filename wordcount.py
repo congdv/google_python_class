d91905d6fc18f1e9cfb7ec6e1d2f1e47d0ff2bc2
congdv <congdaovan94@gmail.com>
2016-01-18 22:59:40 +0700

    add topcount in wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python2.7
+import re
 import sys
-def read_file(file_name):
+import time
+def values(word_count):
+    return word_count[1]
+def read_file(file_name,sorted_count=False):
     word_count = {}
     f = open(file_name,'r')
     for line in f:
         words = line.rstrip('\n').split(' ')
         for word in words:
-            if word not in word_count.keys():
-                word_count[word] = 1
-            else:
-                word_count[word] = word_count[word] + 1
-    for word in sorted(word_count):
-        print word + ' %d'%word_count[word]
+            word = re.search('[a-zA-Z]+',word)
+            if word:
+                word = word.group()
+                if word not in word_count.keys():
+                    word_count[word] = 1
+                else:
+                    word_count[word] = word_count[word] + 1
+    if sorted_count:
+        for word,count in sorted(word_count.items(),key=values,reverse=True):
+            print word +' %d'%count
+
+    else:
+        for word in sorted(word_count):
+            print word + ' %d'%word_count[word]
 def main():
     if len(sys.argv) == 2:
         read_file(sys.argv[1])
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == '--topcount':
+            read_file(sys.argv[2],True)
+        else:
+            print 'False input'
     else:
         read_file('blogger.txt')
 
